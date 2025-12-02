ef0f453fec165873cef0059f8790a5554c4e1ddf
Will Roden <will@roden.cc>
2025-12-02 12:16:24 -0600

    update

--- cmd/overexported/overexported.go
+++ cmd/overexported/overexported.go
@@ -13,6 +13,46 @@ import (
 	"github.com/willabides/overexported/internal/overexported"
 )
 
+const description = `
+The overexported command reports exported identifiers that could be unexported.
+
+The overexported command loads a Go program from source then uses Rapid Type
+Analysis (RTA) to build a call graph of all the functions reachable from the
+program's main function. Any exported identifiers (functions, types, methods,
+variables, and constants) that are not referenced from outside their package
+are reported as over-exported, grouped by package.
+
+Packages are expressed in the notation of 'go list' (or other underlying build
+system if you are using an alternative golang.org/x/go/packages driver). Only
+executable (main) packages are considered starting points for the analysis.
+
+The --test flag causes it to analyze test executables too. Tests sometimes make
+use of identifiers that would otherwise appear to be over-exported, and public
+API identifiers reported as over-exported with --test indicate possible gaps in
+your test coverage or truly unnecessary exports.
+
+The --filter flag restricts results to packages that match the provided regular
+expression; its default value is the special string "<module>" which matches
+the listed packages and any other packages belonging to the same modules. Use
+--filter= to display all results.
+
+Example: show all over-exported identifiers within a module:
+
+  $ overexported --test ./...
+
+By default, the tool does not report exports in generated files, as determined
+by the special comment described in https://go.dev/s/generatedcode . Use the
+--generated flag to include them.
+
+Just because an identifier is reported as over-exported does not mean it is
+unconditionally safe to unexport it. For example, an over-exported function may
+be referenced by another over-exported function. Some judgement is required.
+
+The analysis is valid only for a single GOOS/GOARCH configuration, so an
+identifier reported as over-exported may be used in a different configuration.
+Consider running the tool once for each configuration of interest.
+`
+
 type cliOptions struct {
 	Chdir     string   `short:"C" help:"Change to this directory before running."`
 	Test      bool     `help:"Include test packages and executables in the analysis."`
@@ -33,7 +73,7 @@ func main() {
 func run(stdout io.Writer, args []string) error {
 	var cli cliOptions
 	p, err := kong.New(&cli,
-		kong.Description("Find exported Go identifiers that are not used outside their package and could be unexported."),
+		kong.Description(strings.TrimSpace(description)),
 	)
 	if err != nil {
 		return err
